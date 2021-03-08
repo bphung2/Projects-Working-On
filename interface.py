73a060c7f639555916ea8e145c1e5a66c5e8933e
bphung2 <59210787+bphung2@users.noreply.github.com>
2021-03-08 09:46:35 -0500

    Add files via upload

---
+++ interface.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+window= tk.Tk()
+greeting = tk.Label(text='Hello, Tinker',
+            foreground= 'black', background='white', height=10, width=10)
+greeting.pack()
+window.mainloop()
